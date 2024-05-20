c4973b596eebdbed955b1b36fbb8a54048dc4433
hammadsadi <hammad.sadi@yahoo.com>
2024-05-20 16:53:13 +0600

    Loaded Rooms Details Data

--- src/components/Home/Rooms.jsx
+++ src/components/Home/Rooms.jsx
@@ -3,14 +3,13 @@ import Container from "../Shared/Container";
 import Heading from "../Shared/Heading";
 import LoadingSpinner from "../Shared/LoadingSpinner";
 import { useQuery } from "@tanstack/react-query";
-import { axiosSecure } from "../../hooks/useAxiosSecure";
+import { axiosCommon } from "../../hooks/useAxiosCommon";
 
 const Rooms = () => {
-  // const [rooms, setRooms] = useState([]);
   const { data: rooms = [], isLoading } = useQuery({
     queryKey: ["rooms"],
     queryFn: async () => {
-      const { data } = await axiosSecure.get("/rooms");
+      const { data } = await axiosCommon.get("/rooms");
       return data;
     },
   });
